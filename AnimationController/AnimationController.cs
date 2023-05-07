791a136c855e04b3f31f1bfbde49f28f73d85a76
明明鸭 <86056701+mmyo456@users.noreply.github.com>
2023-05-08 05:08:35 +0800

    Add files via upload

---
+++ AnimationController/AnimationController.cs
@@ -0,0 +1,31 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+// By:明明鸭！
+
+public class AnimationController : UdonSharpBehaviour
+{
+    public Transform targetTransform;  // 要控制的对象的 Transform 组件
+    public AnimationCurve curve;  // 动画曲线
+    public float duration = 1f;  // 动画持续时间
+    public Vector3 startPosition;  // 初始位置
+    public Vector3 endPosition;  // 结束位置
+
+    private float timer = 0f;  // 记录动画已经播放的时间
+
+    public void Update()
+    {
+        // 更新计时器
+        timer += Time.deltaTime;
+
+        // 计算动画进度（0-1之间的值）
+        float progress = Mathf.Clamp01(timer / duration);
+
+        // 使用动画曲线计算位置插值
+        Vector3 position = Vector3.Lerp(startPosition, endPosition, curve.Evaluate(progress));
+
+        // 将位置应用到目标对象的 Transform 组件
+        targetTransform.position = position;
+    }
+}
---
+++ AudioActivation/AudioActivation.cs
@@ -0,0 +1,36 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+//By：明明鸭！
+
+public class AudioActivation : UdonSharpBehaviour
+{
+    public AudioSource audioSource1; // 音频源1
+    public AudioSource audioSource2; // 音频源2
+    public Transform transformToToggle;
+
+    private bool isAudioActive = true;
+
+    private void Start()
+    {
+        isAudioActive = audioSource1.isPlaying || audioSource2.isPlaying;
+        ToggleTransform();
+        transformToToggle.gameObject.SetActive(true); // 将层级更改为默认开启状态
+    }
+
+    private void Update()
+    {
+        bool newAudioActive = audioSource1.isPlaying || audioSource2.isPlaying;
+        if (newAudioActive != isAudioActive)
+        {
+            isAudioActive = newAudioActive;
+            ToggleTransform();
+        }
+    }
+
+    private void ToggleTransform()
+    {
+        transformToToggle.gameObject.SetActive(!isAudioActive);
+    }
+}
