4b107552e4ee54207106f38640f93f0af2bf41d6
明明鸭 <86056701+mmyo456@users.noreply.github.com>
2023-05-19 04:19:27 +0800

    Update AudioActivation.cs
    添加了备注，方便您更好的了解代码
    添加了检测的声音源3
    参考：Sonic853

--- AudioActivation/AudioActivation.cs
+++ AudioActivation/AudioActivation.cs
@@ -1,27 +1,31 @@
-﻿using UdonSharp;
+using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
-//By：明明鸭！
 
 public class AudioActivation : UdonSharpBehaviour
 {
-    public AudioSource audioSource1; // 音频源1
-    public AudioSource audioSource2; // 音频源2
+    [Header("鸭鸭讨厌Bug")]
+    
+    [Header("检测的声音源")]
+    public AudioSource audioSource1;
+    public AudioSource audioSource2;
+    public AudioSource audioSource3;
+    [Header("关闭的层级")]
     public Transform transformToToggle;
 
     private bool isAudioActive = true;
 
     private void Start()
     {
-        isAudioActive = audioSource1.isPlaying || audioSource2.isPlaying;
+        isAudioActive = audioSource1.isPlaying || audioSource2.isPlaying || audioSource3.isPlaying;
         ToggleTransform();
-        transformToToggle.gameObject.SetActive(true); // 将层级更改为默认开启状态
+        transformToToggle.gameObject.SetActive(true);  // 将层级更改为默认开启状态
     }
 
     private void Update()
     {
-        bool newAudioActive = audioSource1.isPlaying || audioSource2.isPlaying;
+        bool newAudioActive = audioSource1.isPlaying || audioSource2.isPlaying || audioSource3.isPlaying;
         if (newAudioActive != isAudioActive)
         {
             isAudioActive = newAudioActive;
